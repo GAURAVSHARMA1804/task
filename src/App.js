bd9f16ef65777bc86084ca8979297eeae2f488ef
GAURAV SHARMA <65211415+GAURAVSHARMA1804@users.noreply.github.com>
2022-02-24 20:11:52 +0530

    initial commit
    ...

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Home from "../src/Home";
+import Instruction from "../src/Instuction";
+import React from "react";
+
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch
+} from "react-router-dom";
+
+function App(props) {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/ins" element={<Instruction />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
